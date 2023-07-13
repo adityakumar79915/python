1b2532817151a05f0440800afe4cfe3b9ad8f06b
adityakumar79915 <adityakumar79915@gmail.com>
2023-07-12 22:37:39 -0700

    updated

---
+++ list.py
@@ -0,0 +1,49 @@
+#list is data structure which can hold data in pyhton
+#list is denoted by square bracket[]
+#list is mutable we can data in list 
+#list is ordered
+#list cannot contain key values as dictionary comntain
+#indexing is possible in list
+
+#coomon list methods are:
+'''
+<1>append()
+<2>remove()
+<3>pop()
+<4>extend()
+<5>insert()
+<6>index()
+<6>count()
+<7>reverse()
+'''
+
+'''x=[1,2,3,4,5]
+x.append(6)
+print(x)'''
+
+'''a=[2,4,6,8]
+a.remove(8)
+print(a)'''
+
+'''x=[12,23,3,4,5,6]
+x.pop()
+print(x)'''
+
+'''x=[1,2,3,4,5]
+x.extend(4)
+print(x)'''
+
+'''a=[1,2,3,4]
+a.insert(4,5)
+print(a)'''
+
+'''x=[2,4,6,8]
+y=x.index(4)
+print(y)'''
+
+'''x=[1,2,3,4,5,6,6,6]
+print(x.count(6))'''
+
+x=[1,2,3,4,5]
+y=x.reverse()
+print(y)
