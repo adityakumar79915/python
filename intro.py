fce88120db88319dd0e195e3518fcfdf030035ed
adityakumar79915 <adityakumar79915@gmail.com>
2023-07-12 22:40:08 -0700

    updated

---
+++ intro.py
@@ -0,0 +1,60 @@
+'''a=int(input("enter a number"))
+print(a*a)'''
+
+#operators
+'''<1>
+Arithmetic operator: +,-,/,%,*,//
+'''
+'''a=5
+b=6
+print(a+b)
+print(a/b)
+print(a*b)  
+print(a//b)
+print(a%b)'''
+
+
+'''<2>
+Logical operator: &,or,not
+'''
+
+'''a=5
+b=10
+print(a or b)'''
+
+
+'''<3>
+Comparision operator: ==,!=,>=,<=
+'''
+'''a=5
+b=8
+print(a == b)
+print(a != b)
+print(a >= b)
+print(a <= b)'''
+
+
+'''<4>
+membership operator: in or not in
+'''
+'''a=[2,4,6,8,6,4]
+print(4 in a)
+print(9 in a)'''
+
+'''<5>
+Assignment operator : +=,-+,*=,/=,//=
+a=5
+b=5'''
+
+'''<6>
+identity operator: is,is not
+'''
+x=6
+y=8
+z= x is y
+print(z)
+c=x is not y
+print(c)
+
+
+
